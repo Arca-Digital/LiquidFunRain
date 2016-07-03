295c21f2e774aab483021c20839f7d58793a8b79
Arca-Digital <developer@arcadigital.co>
2016-07-03 10:50:07 +1000

    Creating a sheet of liquid particles
    Added a particle group in the shape of a box, in liquidfun
    
    created a translucent sprite for each of the particles, assigning it a
       random color.

--- js/demo.js
+++ js/demo.js
@@ -2,17 +2,64 @@
 
 const WINDOW_WIDTH = 400;
 const WINDOW_HEIGHT = 460;
+const METER = 100;
+
+var sprites;
+var world;
+
+var meterToPixel = (mx) => mx*METER;
+var pixelToMeter = (px) => px/METER;
+
+function createParticleGroup() {
+    var psd = new b2ParticleSystemDef();
+    psd.radius = 0.025;
+    psd.dampingStrength = 0.2;
+    var particleSystem = world.CreateParticleSystem(psd);
+    var box = new b2PolygonShape();
+    box.SetAsBoxXYCenterAngle(
+        1.25, 1.25,
+        new b2Vec2(pixelToMeter(WINDOW_WIDTH) / 2, 2.0),
+        0
+    );
+    var particleGroupDef = new b2ParticleGroupDef();
+    particleGroupDef.shape = box;
+    var particleGroup = particleSystem.CreateParticleGroup(particleGroupDef);
+}
+
+function InitializeRainMaker() {
+	createParticleGroup();
+}
 
 (function init(divName) {
 	// define gravity in LiquidFun and initialize world
     let gravity = new b2Vec2(0, 10);
-    let world = new b2World(gravity);
+    world = new b2World(gravity);
 	
 	// create minimal phaser.js game:
+	sprites = [];
     let game = new Phaser.Game(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.AUTO, divName, {
 		preload: () => {
+			game.load.image('dot', 'img/particles.png');
+			InitializeRainMaker();
 		},
-		create: () => {			
+		create: () => {
+			let SetupParticles = function() {
+				let particles = world.particleSystems[0].GetPositionBuffer();
+				for (let i = 0; i < particles.length/2; i++)
+				{
+					let x = meterToPixel(particles[i * 2]);
+					let y = meterToPixel(particles[(i * 2) + 1]);
+					sprites[i] = game.add.sprite(x,y, 'dot');
+					sprites[i].anchor.set(0.5, 0.5);
+
+					let color = Math.floor(Math.random()*256)*0x10000
+							+ Math.floor(Math.random()*256)*0x100
+							+ Math.floor(Math.random()*256);  // RGB random colors
+					sprites[i].tint = color;
+				}
+			}
+
+			SetupParticles();
 		},
 		update: () => {
 		}
